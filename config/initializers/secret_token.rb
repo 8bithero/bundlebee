4a0d95ada7e7c585d20d5872f6577601a6e76bfb
8Bit Hero <pkemanes@gmail.com>
2012-10-25 11:06:56 +0300

    Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bundlebee::Application.config.secret_token = 'a0a6a7254a86d579f5aa8ab89024caff7dc33721498618cc0ffdbfd9d364d2eb9176d1d93c75a4170e06893d5bd9f3750663332b119dd5a7374bf3bec8190f8a'
